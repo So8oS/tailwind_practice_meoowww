02449430817a9eb359cf5a1b4c50cce0d51d46c0
Ahmad Othman <62267611+So8oS@users.noreply.github.com>
2022-12-20 19:37:38 +0400

    Done

--- src/componants/navbar.jsx
+++ src/componants/navbar.jsx
@@ -5,9 +5,9 @@ export const Navbar = () => {
     <div className="bg-white drop-shadow-2xl ">
       <div className="relative container mx-auto p-5 bg-white   ">
         <div className="flex items-center justify-between">
-          <h1 className="font-bold text-4xl	font-Caveat ">Animals</h1>
-          <div className="hidden md:flex space-x-6  ">
-            <a className="text-3xl	font-Caveat" href="https://unsplash.com/s/photos/cats">
+          <h1 className="font-bold text-4xl	font-Caveat ">MEOOWWW </h1>
+          <div className=" md:flex space-x-6  ">
+            <a className="text-3xl	font-Caveat" target="_blank" href="https://unsplash.com/s/photos/cats">
               Source
             </a>
           </div>
